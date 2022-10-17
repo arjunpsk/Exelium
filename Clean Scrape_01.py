e31348b4c1ea39a83a8bd969d95f8b0133e705db
arjunpsk <arjun.pavakkulath@gmail.com>
2022-10-18 01:29:33 +0200

    First Deliverable

--- Clean Scrape_01.py
+++ Clean Scrape_01.py
@@ -4,11 +4,13 @@ Commented to show all the elements I need to scrape from the website.
 
 Root element of each listing = ef447dde
 
+XPATHS of Elements
 Name = //*[@id="body-wrapper"]/main/div[2]/div[2]/div[2]/div[1]/div[2]/div/ul/li[1]/article/div[3]/div[3]/div[1]
 Price = //*[@id="body-wrapper"]/main/div[2]/div[2]/div[2]/div[1]/div[2]/div/ul/li[1]/article/div[3]/div[2]/div/span[3]
 Sqft = //*[@id="body-wrapper"]/main/div[2]/div[2]/div[2]/div[1]/div[2]/div/ul/li[1]/article/div[3]/div[3]/div[3]/div/div/span[3]/span[2]
 URL = //*[@id="body-wrapper"]/main/div[2]/div[2]/div[2]/div[1]/div[2]/div/ul/li[1]/article/div[1]/a
 
+CLASS NAMES of Elements
 Name = class="_7afabd84"
 Price = class="f343d9ce"
 Sqft = class="b6a29bc0"
@@ -19,7 +21,12 @@ links = wait.until(EC.presence_of_element_located((By.CLASS_NAME , "_357a9937"))
 """
 
 
+from array import array
+from pickle import APPEND
 import pandas as pd
+import numpy as np
+from IPython.display import display
+
 
 # waiting
 from selenium.webdriver.support.ui import WebDriverWait
@@ -42,79 +49,77 @@ driver = webdriver.Chrome(service=service, options=options)
 # Variable to set the URL
 url = "https://www.bayut.com/for-sale/property/dubai/jumeirah-lake-towers-jlt/"
 
-Name = '//*[@id="body-wrapper"]/main/div[2]/div[2]/div[2]/div[1]/div[2]/div/ul/li[1]/article/div[3]/div[3]/div[1]'
-Price = '//*[@id="body-wrapper"]/main/div[2]/div[2]/div[2]/div[1]/div[2]/div/ul/li[1]/article/div[3]/div[2]/div/span[3]'
-Sqft = '//*[@id="body-wrapper"]/main/div[2]/div[2]/div[2]/div[1]/div[2]/div/ul/li[1]/article/div[3]/div[3]/div[3]/div/div/span[3]/span[2]'
-URL = '//*[@id="body-wrapper"]/main/div[2]/div[2]/div[2]/div[1]/div[2]/div/ul/li[1]/article/div[1]/a'
-
 # Function to scrape from URL
 
 def open_chrome_new():
-    name = []
-    driver.get(url)
-    wait = WebDriverWait(driver, 10)
+    try:    
+        name = []
+        driver.get(url)
+        wait = WebDriverWait(driver, 10)
 
-    driver.maximize_window() # For maximizing window
-    driver.implicitly_wait(10) # gives an implicit wait for 20 seconds
+        driver.maximize_window() # For maximizing window
+        driver.implicitly_wait(10) # gives an implicit wait for 20 seconds
+        
+        # listings = driver.find_elements(By.CLASS_NAME, "ef447dde").text
+        listings = wait.until(EC.presence_of_element_located((By.CLASS_NAME , "_357a9937"))).find_elements(By.CLASS_NAME, "ef447dde") # works
     
-    # listings = driver.find_elements(By.CLASS_NAME, "ef447dde").text
-    listings = wait.until(EC.presence_of_element_located((By.CLASS_NAME , "_357a9937"))).find_elements(By.CLASS_NAME, "ef447dde") # works
-    # listings = wait.until(EC.presence_of_element_located((By.NAME , "_357a9937"))).find_elements(By.NAME, "ef447dde")
-   
-    print(listings)
-    print("HELLO WORLD - HELLO WORLD - HELLO WORLD - HELLO WORLD - HELLO WORLD - HELLO WORLD - HELLO WORLD - HELLO WORLD")
+        print("The number of properties found:", len(listings), end="\n\n\n\n\n\n")
 
-    for listing in listings:
-        list_name = listing.find_element(By.XPATH, '//*[@id="body-wrapper"]/main/div[2]/div[2]/div[2]/div[1]/div[2]/div/ul/li[1]/article/div[3]/div[3]/div[1]')
-        list_price = listing.find_element(By.XPATH, '//*[@id="body-wrapper"]/main/div[2]/div[2]/div[2]/div[1]/div[2]/div/ul/li[1]/article/div[3]/div[2]/div/span[3]').text
-        list_sqft = listing.find_element(By.XPATH, '//*[@id="body-wrapper"]/main/div[2]/div[2]/div[2]/div[1]/div[2]/div/ul/li[1]/article/div[3]/div[3]/div[3]/div/div/span[3]/span[2]').get_attribute(".b6a29bc0")
-        list_url = listing.find_element(By.XPATH, '//*[@id="body-wrapper"]/main/div[2]/div[2]/div[2]/div[1]/div[2]/div/ul/li[1]/article/div[1]/a').get_attribute("href")
-        # print(list_name, list_price, list_sqft, list_url)
-        print(listing, list_price, list_url, end="\n")
-        # list_url = listing.find_element(By.NAME, "_287661cb")
+        print("This is just decorative for the sake of easy readability: \n\n\n*************** STARTS FROM HERE ***************", end="\n\n\n")
 
+        # Define list variables 
+        sqft_temp = []
+        details_of_listing = []
 
-        # print(list_name, list_price, list_sqft, list_url, "NEXT", len(listings))
-        # print(list_url)
+        for listing in listings:
+            wait.until(EC.presence_of_element_located((By.CLASS_NAME, '_7afabd84')))
+
+            List_dict={
+            'Name':listing.find_element(By.CLASS_NAME, '_7afabd84').text,
+            'Price':listing.find_element(By.CLASS_NAME, 'f343d9ce').text,
+            'URL':listing.find_element(By.CLASS_NAME, '_287661cb').get_attribute("href")
+            }
+
+            list_sqft = listing.find_elements(By.CLASS_NAME, 'b6a29bc0')
+
+            details_of_listing.append(List_dict)
+
+            for element in list_sqft:
+                sqft_temp.append(element.text)
+                
+        list_sqft = np.array(sqft_temp).reshape(-1,3)    # reshape the sqft list to an array of 3 columns.
+        
+        df_list = pd.DataFrame(details_of_listing, columns=['Name', 'Price', 'URL'])
+        df_sqft = pd.DataFrame(list_sqft, columns = ['Room', 'Bath', 'Sqft'])
+
+        df_all_details = pd.concat([df_list,df_sqft], axis=1)
+        display(df_all_details)
+
+        df_all_details['Sqft'] = df_all_details['Sqft'].map(lambda x: x.rstrip('sqft'))
+        df_all_details['Price'] = df_all_details['Price'].replace(',','', regex=True)
+        df_all_details['Sqft'] = df_all_details['Sqft'].replace(',','', regex=True)
+
+        display(df_all_details.info())
+
+        print("This is just decorative for the sake of easy readability: HELLO WORLD - MIC CHECK ONE OH - GOODBYE WORLD", end="\n\n\n")
+
+        df_all_details[["Price", "Bath", "Sqft"]] = df_all_details[["Price", "Bath", "Sqft"]].apply(pd.to_numeric)
+        display(df_all_details.info())
+        display(df_all_details)
+
+        print("This is just decorative for the sake of easy readability: HELLO WORLD - MIC CHECK ONE OH - GOODBYE WORLD", end="\n\n\n")
+
+        df_all_details["Price/sqft"] = (df_all_details["Price"]/df_all_details["Sqft"]).round(decimals=2)
+        display(df_all_details)
+
+        # saving the dataframe 
+        df_all_details.to_csv('./result.csv') 
         
     
-    # for listing in listings:
-    #     list_name = listing.find_element(By.XPATH, Name).text
-    #     list_price = listing.find_element(By.
+    finally: # regardsless of outcome above kill the driver because unclosed drivers are killing my flow.
 
-    #     list_price = listing.find_element(By.XPATH, Price).text
-    #     list_sqft =listing.find_element(By.XPATH, Sqft).text
-    #     list_url = listing.find_element(By.XPATH, URL).text
-
-    # try:
-
-    #     wait = WebDriverWait(driver, 10)
-    #     # elements = wait.until(EC.presence_of_element_located((By.ID , "table-rows"))).find_elements_by_css_selector("tr>td>a")
-    #     # links = wait.until(EC.presence_of_element_located((By.CLASS_NAME , "_357a9937"))).find_elements(By.CSS_SELECTOR, "li>article>._4041eb80>a")
-    #     prices = wait.until(EC.presence_of_element_located((By.CLASS_NAME , "ef447dde"))).find_elements(By.CLASS_NAME, "li>article>._4041eb80>a")
-
-    #     # f343d9ce
-    #     print(prices)
-    #     urls = []
-    #     prices = []
-    #     for element in prices:
-    #         print(element.get_attribute("href"))
-    #         print(element.text)
-    #         urls.append(element.get_attribute("href"))
-    #     # dictionary of lists  
-    #     # dict = {'price': prices, 'website': urls}  
-    #     # for element in prices:
-    #     #     print(element.text)
-    #     # df = pd.DataFrame(dict) 
-            
-    #     # # saving the dataframe 
-    #     # df.to_csv('./result.csv') 
-       
-    # finally:
-    #     driver.quit()
-    driver.implicitly_wait(10) # gives an implicit wait for 20 seconds
-
-    driver.close()
-    driver.quit()
+        driver.implicitly_wait(10) # gives an implicit wait for 20 seconds 
+        driver.close()
+        driver.quit()
     
 open_chrome_new()
